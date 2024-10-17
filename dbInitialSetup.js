957a431e49cfd6a0e6f8b49bece89d5d1c14a5f2
Gregory Hunkeler <gregoryhunkeler@gmail.com>
2024-10-17 11:36:04 -0300

    add users seeders

--- dbInitialSetup.js
+++ dbInitialSetup.js
@@ -4,10 +4,7 @@ module.exports = async () => {
   // Crear tablas:
   await db.sequelize.sync({ force: true });
   console.log("[Database] ¡Las tablas fueron creadas!");
-  // await require("./seeders/categorySeeders")();
-  // await require("./seeders/productSeeders")();
-  // await require("./seeders/userSeeders")();
-  // await require("./seeders/orderSeeders")();
+  await require("./seeders/usersSeeders")();
 
   console.log("[Database] ¡Los datos de prueba fueron insertados!");
 };
---
+++ seeders/usersSeeders.js
@@ -0,0 +1,60 @@
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+
+module.exports = async () => {
+  const users = [
+    {
+      first_name: "Gregory",
+      last_name: "Hunkeler",
+      email: "gregoryhunkeler@gmail.com",
+      password: "banana",
+      isAdmin: true,
+    },
+    {
+      first_name: "Admin",
+      last_name: "Admin",
+      email: "Admin@Admin.com",
+      password: "admin",
+      isAdmin: true,
+    },
+    {
+      first_name: "Maria",
+      last_name: "Perez",
+      email: "maria@gmail.com",
+      password: "banana",
+      isAdmin: false,
+      address: "Coronel Perin",
+      phone_number: "1568555215",
+    },
+    {
+      first_name: "Paula",
+      last_name: "Gonzales",
+      email: "paula@gmail.com",
+      password: "banana",
+      isAdmin: false,
+      address: "bv Artigas",
+      phone_number: "9999999",
+    },
+    {
+      first_name: "Pedro",
+      last_name: "Fonsa",
+      email: "pedro@gmail.com",
+      password: "banana",
+      isAdmin: false,
+      address: "Av Brazil",
+      phone_number: "123123123",
+    },
+    {
+      first_name: "Alina",
+      last_name: "Gabriels",
+      email: "alina_gabriels@gmail.com",
+      password: "banana",
+      isAdmin: false,
+      address: "18st",
+      phone_number: "099123123",
+    },
+  ];
+
+  await User.bulkCreate(users);
+  console.log("[Database] Se corrió el seeder de Users.");
+};
